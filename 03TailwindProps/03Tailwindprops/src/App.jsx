4a9fb48e97329f18d1a5b7122a26203fa485bc6a
vikartanSood <soodvikartan@gmail.com>
2023-09-22 23:51:09 +0530

    components added

--- 03TailwindProps/03Tailwindprops/src/App.jsx
+++ 03TailwindProps/03Tailwindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,13 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind CSS</h1>
+
+      <Card/>
+      <Card/>
+      <Card/>
+
+    
+
     </>
   )
 }
---
+++ 03TailwindProps/03Tailwindprops/src/components/card.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div class="mx-auto max-w-7xl px-4 md:px-8 2xl:px-16">
+
+  <div class="block grid-cols-9 items-start gap-x-10 pb-10 pt-7 lg:grid lg:pb-14 xl:gap-x-14 2xl:pb-20">
+    <div class="col-span-5 grid grid-cols-2 gap-2.5">
+      <div class="col-span-1 transition duration-150 ease-in hover:opacity-90">
+        <img
+          src="https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXN8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="Nike Air Max 95 By You--0"
+          class="w-full object-cover"
+        />
+      </div>
+      <div class="col-span-1 transition duration-150 ease-in hover:opacity-90">
+        <img
+          src="https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXN8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="Nike Air Max 95 By You--0"
+          class="w-full object-cover"
+        />
+      </div>
+      <div class="col-span-1 transition duration-150 ease-in hover:opacity-90">
+        <img
+          src="https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXN8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="Nike Air Max 95 By You--0"
+          class="w-full object-cover"
+        />
+      </div>
+      <div class="col-span-1 transition duration-150 ease-in hover:opacity-90">
+        <img
+          src="https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2hvZXN8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="Nike Air Max 95 By You--0"
+          class="w-full object-cover"
+        />
+      </div>
+    </div>
+    <div class="col-span-4 pt-8 lg:pt-0">
+      <div class="mb-7 border-b border-gray-300 pb-7">
+        <h2 class="text-heading mb-3.5 text-lg font-bold md:text-xl lg:text-2xl 2xl:text-3xl">
+          Nike Air Max 95 By You
+        </h2>
+        <p class="text-body text-sm leading-6  lg:text-base lg:leading-8">
+          The Nike Air Max 95 By You lets you take inspiration from the human
+          body itself. The midsole represents the spine, graduated panels are
+          the muscles, the lace loops are the shoe&#x27;s ribs and the mesh in
+          the upper is the skin.
+        </p>
+        <div class="mt-5 flex items-center ">
+          <div class="text-heading pr-2 text-base font-bold md:pr-0 md:text-xl lg:pr-2 lg:text-2xl 2xl:pr-0 2xl:text-4xl">
+            $40.00
+          </div>
+          <span class="font-segoe pl-2 text-sm text-gray-400 line-through md:text-base lg:text-lg xl:text-xl">
+            $50.00
+          </span>
+        </div>
+      </div>
+      <div class="border-b border-gray-300 pb-3  ">
+        <div class="mb-4">
+          <h3 class="text-heading mb-2.5 text-base font-semibold capitalize md:text-lg">
+            size
+          </h3>
+          <ul class="colors -mr-3 flex flex-wrap">
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm ">
+              S
+            </li>
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm ">
+              M
+            </li>
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm ">
+              L
+            </li>
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm ">
+              XL
+            </li>
+          </ul>
+        </div>
+        <div class="mb-4 ">
+          <h3 class="text-heading mb-2.5 text-base font-semibold capitalize md:text-lg">
+            color
+          </h3>
+          <ul class="colors -mr-3 flex flex-wrap">
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm">
+              <span class="block h-full w-full rounded bg-orange-400"></span>
+            </li>
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm">
+              <span class="block h-full w-full rounded bg-pink-400"></span>
+            </li>
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm">
+              <span class="block h-full w-full rounded bg-violet-600"></span>
+            </li>
+            <li class="text-heading mb-2 mr-2 flex h-9 w-9 cursor-pointer items-center justify-center rounded border border-gray-100 p-1 text-xs font-semibold uppercase transition duration-200 ease-in-out hover:border-black md:mb-3 md:mr-3 md:h-11 md:w-11 md:text-sm">
+              <span class="block h-full w-full rounded bg-red-500"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="space-s-4 3xl:pr-48 flex items-center gap-2 border-b border-gray-300 py-8  md:pr-32 lg:pr-12 2xl:pr-32">
+        <div class="group flex h-11 flex-shrink-0 items-center justify-between overflow-hidden rounded-md border border-gray-300 md:h-12">
+          <button
+            class="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-e border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12"
+            disabled=""
+          >
+            +
+          </button>
+          <span class="duration-250 text-heading flex h-full w-12  flex-shrink-0 cursor-default items-center justify-center text-base font-semibold transition-colors ease-in-out  md:w-20 xl:w-24">
+            1
+          </span>
+          <button class="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-s border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12">
+            -
+          </button>
+        </div>
+        <button
+          type="button"
+          class="h-11 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        >
+          Add to cart
+        </button>
+      </div>
+ 
+    </div>
+  </div>
+</div>
+
+
+  )
+}
+
+export default Card
