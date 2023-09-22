d7fcf2d345346aba6f0da42833d45c2be1e505c5
vikartanSood <soodvikartan@gmail.com>
2023-09-23 00:22:08 +0530

    card changed

--- 03TailwindProps/03Tailwindprops/src/App.jsx
+++ 03TailwindProps/03Tailwindprops/src/App.jsx
@@ -5,17 +5,13 @@ import './App.css'
 import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind CSS</h1>
 
-      <Card/>
-      <Card/>
-      <Card/>
-
-    
+      <Card shoebrand= "Nike"/>
+      <Card shoebrand= "Adidas" src = "https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YWRpZGFzJTIwc2hvZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"/>
 
     </>
   )
